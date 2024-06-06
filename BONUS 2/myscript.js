85c643cbf7cbcdae674e361e72961efde40be228
LorisGalleana <loris.galleana@gmail.com>
2024-06-06 17:45:56 +0200

    ESERCIZIO BONUS 3 COMPLETATO

--- BONUS 2/myscript.js
+++ BONUS 2/myscript.js
@@ -3,8 +3,6 @@ const fizzbuzz = document.querySelector(".container");
 for (let i=1; i <= 100; i++) {
     //creo costante che quando chiamata crei un elemento div dove richiesto
     const elementBox = document.createElement("div");
-    //aggiungo all'elemento "elementBox" la variabile "i"
-    elementBox.append(i);
     //aggiungo a tutti gli "elementBox" la classe box
     elementBox.classList.add("box")
 
