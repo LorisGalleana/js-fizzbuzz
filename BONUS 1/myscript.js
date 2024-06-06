2ff30d9c8b40525600bcfc28661ce06413052cbd
LorisGalleana <loris.galleana@gmail.com>
2024-06-06 17:25:19 +0200

    ESRCIZIO BONUS 1 COMPLETATO

--- BONUS 1/myscript.js
+++ BONUS 1/myscript.js
@@ -3,8 +3,27 @@ const fizzbuzz = document.querySelector(".container");
 for (let i=1; i <= 100; i++) {
     //creo costante che quando chiamata crei un elemento div dove richiesto
     const elementBox = document.createElement("div");
+    if ( i % 15 === 0) {
+        elementBox.innerHTML = "fizzbuzz";
+        console.log("fizzbuzz");
+    }
+    //creo una condizione per cui al posto della variabile "i", il codice scriva in console la parola "fizz" per i numeri che sono multipli di 3 
+    else if ( i % 3 === 0) {
+        elementBox.innerHTML = "fizz";
+        console.log("fizz");
+    }
+    //creo una condizione per cui al posto della variabile "i", il codice scriva in console la parola "buzz" per i numeri che sono multipli di 5 
+    else if ( i % 5 === 0) {
+        elementBox.innerHTML = "buzz";
+        console.log("buzz");
+    }
+    //creo una condizione per cui al posto della variabile "i", il codice scriva in console il valore della var "i" per tutti i numeri che non rientrano nelle condizioni precedente 
+    else  {
+        elementBox.append(i)
+        console.log(i);
+    }
     //aggiungo all'elemento "elementBox" la variabile "i"
-    elementBox.append(i);
+    //elementBox.append(i);
     // aggiungo all'interno dell'elemento "fizzbuzz" la variabile "elementBox"
     fizzbuzz.appendChild(elementBox);
 
